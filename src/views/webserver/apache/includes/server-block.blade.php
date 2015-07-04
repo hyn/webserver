6a46b79e7d9d37452184d8879b739c0102cb08e6
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-04 08:22:20 +0200

    rewrite of twig to blade templates

---
+++ src/views/webserver/apache/includes/server-block.blade.php
@@ -0,0 +1,100 @@
+
+
+<VirtualHost *:{{ $config->port->http or 80 }}>
+    @if($hostname or false)
+        ServerAdmin webmaster@{{ $hostname->hostname }}
+    @else
+        ServerAdmin webmaster@{{ $hostnames->first()->hostname }}
+    @endif
+
+    @if($hostname or false)
+        ServerName {{ $hostname->hostname }}
+    @else
+        @foreach($hostnames->lists('hostname') as $i => $hostname)
+            @if($i == 0)
+                ServerName {{ $hostname }}
+            @else
+                ServerAlias {{ $hostname }}
+            @endif
+        @endforeach
+    @endif
+
+    # public path, serving content
+    DocumentRoot {{ public_path() }}
+    # default document handling
+    DirectoryIndex index.html index.php
+
+    # user
+    #    RUidGid {{ $website->identifier }} users
+
+    @if($website->directory->media)
+        # media directory
+        alias "/media/" "{{ $website->directory->media }}"
+    @endif
+
+    # allow cross domain loading of resources
+    Header set Access-Control-Allow-Origin "*"
+
+    # logging
+    ErrorLog {{ $log_path }}.error.log
+    CustomLog {{ $log_path }}.access.log combined
+</VirtualHost>
+
+
+@if($ssl or false)
+<VirtualHost *:{{ $config->port->https or 443 }}>
+
+    @if($hostname or false)
+        ServerAdmin webmaster@{{ $hostname->hostname }}
+    @else
+        ServerAdmin webmaster@{{ $hostnames->first()->hostname }}
+    @endif
+
+    @if($hostname or false)
+        ServerName {{ $hostname->hostname }}
+    @else
+        @foreach($hostnames->lists('hostname') as $i => $hostname)
+            @if($i == 0)
+                ServerName {{ $hostname }}
+            @else
+                ServerAlias {{ $hostname }}
+            @endif
+        @endforeach
+    @endif
+
+    # public path, serving content
+    DocumentRoot {{ public_path() }}
+    # default document handling
+    DirectoryIndex index.html index.php
+
+    # user
+    #    RUidGid {{ $website->identifier }} users
+
+    @if($website->directory->media)
+        # media directory
+        alias "/media/" "{{ $website->directory->media }}"
+    @endif
+
+    # allow cross domain loading of resources
+    Header set Access-Control-Allow-Origin "*"
+
+    # logging
+    ErrorLog {{ $log_path }}.error.log
+    CustomLog {{ $log_path }}.access.log combined
+
+    # enable SSL
+    SSLEngine On
+    SSLCertificateFile {{ $ssl->pathPem }}
+    SSLCertificateKeyFile {{ $ssl->pathKey }}
+
+    <FilesMatch "\.(cgi|shtml|phtml|php)$">
+    SSLOptions +StdEnvVars
+    </FilesMatch>
+
+    BrowserMatch "MSIE [2-6]" \
+    nokeepalive ssl-unclean-shutdown \
+    downgrade-1.0 force-response-1.0
+    # MSIE 7 and newer should be able to use keepalive
+    BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
+</VirtualHost>
+@endif
--- src/views/webserver/fpm/configuration.blade.php
+++ src/views/webserver/fpm/configuration.blade.php
@@ -1,18 +1,18 @@
 ;#
 ;#   Auto generated Nginx configuration
-;#       @time: {{ "now"|date('H:i:s d-m-Y') }}
+;#       @time: {{ date('H:i:s d-m-Y') }}
 ;#       @author: hyn.me
-;#       @website: {{ website.id }} "{{ website.present().name }}"
+;#       @website: {{ $website->id }} "{{ $website->present()->name }}"
 ;#
 
 ;# unique fpm group
-[{{ website.id }}-{{ website.present().urlName }}]
+[{{ $website->id }}-{{ $website->present()->urlName }}]
 
 ;# listening for nginx proxying
-listen=127.0.0.1:{{ config.port + website.id }}
+listen=127.0.0.1:{{ $config['port'] + $website->id }}
 
 ;# user under which the application runs
-user={{ user }}
+user={{ $user }}
 
 ;# group under which the application runs
 group=users
@@ -26,4 +26,4 @@ pm.max_spare_servers    = 10
 pm.max_requests         = 20
 
 ;# force fpm workers into the following path
-chdir                   = {{ base_path }}
+chdir                   = {{ base_path() }}
