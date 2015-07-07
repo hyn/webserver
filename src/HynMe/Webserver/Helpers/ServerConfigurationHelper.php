e1f8cba8b233a63a907da9969395fbd6eedabcdf
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-07-07 06:14:41 +0200

    creates directories for webserver

--- src/HynMe/Webserver/Helpers/ServerConfigurationHelper.php
+++ src/HynMe/Webserver/Helpers/ServerConfigurationHelper.php
@@ -17,4 +17,19 @@ class ServerConfigurationHelper
     {
         return function_exists('posix_getuid') && posix_getuid() == 0 ? 'root' : get_current_user();
     }
+
+    /**
+     * Creates directories if not yet existing
+     * Generated for any configured service in config
+     */
+    public function createDirectories()
+    {
+        foreach(config('webserver') as $key => $params)
+        {
+            $path = array_get($params, 'path');
+
+            if($path && !File::isDirectory($path))
+                File::makeDirectory($path, 0755, true);
+        }
+    }
 }
