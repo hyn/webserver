8e32e75d6fa8bdcbd0c3e21471912d56cc7b599b
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-09-30 07:25:11 +0200

    first steps towards mocking for unit testing the webserver code

--- tests/WebsiteUserTest.php
+++ tests/WebsiteUserTest.php
@@ -1,7 +1,11 @@
 <?php namespace HynMe\Webserver\Tests;
 
+use Laraflock\MultiTenant\Models\Website;
+use Mockery;
 use HynMe\Framework\Testing\TestCase;
 
 class WebsiteUserTest extends TestCase {
-
+    public function testCallCommand() {
+        $mock = new Mockery(Website::class);
+    }
 }
