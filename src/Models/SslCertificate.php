0c93ac4cd18dbb8b781d1a64deeb6f1dee284f95
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-10-18 13:25:28 +0200

    fixed tenant in ssl model and added phpdoc

--- src/Models/SslCertificate.php
+++ src/Models/SslCertificate.php
@@ -6,17 +6,35 @@ use Config;
 use Hyn\Webserver\Tools\CertificateParser;
 use Laracasts\Presenter\PresentableTrait;
 use Laraflock\MultiTenant\Abstracts\Models\SystemModel;
+use Laraflock\MultiTenant\Models\Tenant;
 
+/**
+ * Class SslCertificate
+ *
+ * @package Hyn\Webserver\Models
+ */
 class SslCertificate extends SystemModel
 {
     use PresentableTrait;
 
+    /**
+     * @var string
+     */
     protected $presenter = 'Hyn\Webserver\Presenters\SslCertificatePresenter';
 
-    protected $fillable = ['tenant_id', 'certificate', 'authority_bundle'];
+    /**
+     * @var array
+     */
+    protected $fillable = ['tenant_id', 'certificate', 'authority_bundle', 'key'];
 
+    /**
+     * @var array
+     */
     protected $appends = ['pathKey', 'pathPem' ,'pathCrt', 'pathCa'];
 
+    /**
+     * @return array
+     */
     public function getDates()
     {
         return ['validates_at', 'invalidates_at'];
@@ -30,31 +48,58 @@ class SslCertificate extends SystemModel
         return $this->certificate ? new CertificateParser($this->certificate) : null;
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function hostnames()
     {
         return $this->hasMany(SslHostname::class);
     }
 
+    /**
+     * @param string $postfix
+     * @return string
+     */
     public function publishPath($postfix = 'key')
     {
         return sprintf('%s/%s/certificate.%s', Config::get('webserver.ssl.path'), $this->id, $postfix);
     }
 
+    /**
+     * @return string
+     */
     public function getPathKeyAttribute()
     {
         return $this->publishPath('key');
     }
 
+    /**
+     * @return string
+     */
     public function getPathPemAttribute()
     {
         return $this->publishPath('pem');
     }
 
+    /**
+     * @return string
+     */
     public function getPathCrtAttribute()
     {
         return $this->publishPath('crt');
     }
 
+    /**
+     * @return string
+     */
     public function getPathCaAttribute()
     {
         return $this->publishPath('ca');
