aa1cd9157f38fc14999c9b85530a3abceaa606a7
Daniel Klabbers <daniel+hyn@klabbers.email>
2015-12-08 18:43:45 +0100

    fixed incorrect name of migration class

--- database/migrations/2015_05_19_000002_hws_ssl_hostnames_drop_hostname_id.php
+++ database/migrations/2015_05_19_000002_hws_ssl_hostnames_drop_hostname_id.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Hyn\MultiTenant\Tenant\DatabaseConnection;
 
-class HwsSslHostnamesTableDropHostnameId extends Migration
+class HwsSslHostnamesDropHostnameId extends Migration
 {
     /**
      * Run the migrations.
